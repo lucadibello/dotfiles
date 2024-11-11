39bd4dd48e259ae5e0bcc0272f6aeca7985c91a6
Luca Di Bello <lucadibello@proton.me>
2024-11-11 21:49:15 +0100

    now adding homebrew to fish

--- scripts/install/pre/fish.sh
+++ scripts/install/pre/fish.sh
@@ -5,6 +5,10 @@ if ! command -v fish &>/dev/null; then
   echo "fish is not installed. Installing it now..."
   brew install fish fisher
 
+  # add important paths to fish's PATH env
+  fish_add_path /opt/homebrew/bin
+
+
   if [ $? -ne 0 ]; then
     echo "❌ Failed to install fisher. Exiting..." >&2
     exit 1
