138fda3095e544aab59c1d4da7e820d08cb79365
Luca Di Bello <lucadibello@proton.me>
2024-11-11 21:49:07 +0100

    Now installing useful fish plugins

--- scripts/install/post/fisher-install.sh
+++ scripts/install/post/fisher-install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 # Install useful fisher plugins!
-# fisher install
+
+# Install + configure tide
+fisher install IlanCosman/tide@v6
+tide configure --auto --style=Lean --prompt_colors='16 colors' --show_time=No --lean_prompt_height='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No
+
+# Install autopair
+fisher install jorgebucaran/autopair.fish
